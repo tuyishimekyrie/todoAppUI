22f4dbe881c13f75b5eb1253304d85fb9ada2f94
Hope <85252664+tuyishimekyrie@users.noreply.github.com>
2024-04-03 22:08:27 +0200

    Update NotFoundPage.tsx remove unused codes

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+
 import { Link } from 'react-router-dom'
 
 export default function NotFoundPage() {
   return (
       <div>
           <h1>404 Page Not Found</h1>
-          <button></button>
+          
           <Link to="/">Home</Link>
     </div>
   )
